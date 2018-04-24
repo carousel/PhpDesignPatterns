d3698bd27522fecc1cc7b385d8a9e27e8481caf2
Miroslav Trninic <miroslav.trninic@gmail.com>
2018-04-24 15:32:53 +0200

    adapter abstract

--- src/structural/Adapter/BookAdapter.php
+++ src/structural/Adapter/BookAdapter.php
@@ -4,7 +4,7 @@ namespace Structural\Adapter;
 
 use Helpers\Book;
 
-class BookAdapter
+class BookAdapter extends BookAdapterTarget
 {
     private $book;
 
---
+++ src/structural/Adapter/BookAdapterTarget.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Structural\Adapter;
+
+abstract class BookAdapterTarget
+{
+    abstract public function getAuthorAndTitle();
+}
